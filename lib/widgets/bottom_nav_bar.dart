d893f42b19a2ce1d637e0acd932bb5342baee37f
Shahid-Zada <Shahidzada.cs@gmail.com>
2024-03-24 20:11:00 +0500

    navigation br

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:netflix_clone/screens/home_screen.dart';
+import 'package:netflix_clone/screens/more_screen.dart';
+import 'package:netflix_clone/screens/search_screen.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildCont) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        bottomNavigationBar: Container(
+          child: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: 'Home',
+              ),
+              Tab(
+                icon: Icon(Icons.search),
+                text: 'Search',
+              ),
+              Tab(
+                icon: Icon(Icons.photo_library_outlined),
+                text: 'New & Hot',
+              )
+            ],
+            indicatorColor: Colors.transparent,
+            labelColor: Colors.white,
+            unselectedLabelColor: Color(0xff999999),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            HomeScreen(),
+            SearchScreen(),
+            MoreScreen(),
+          ],
+        ),
+      ),
+    );
+  }
+}
