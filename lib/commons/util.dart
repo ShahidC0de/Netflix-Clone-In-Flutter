ae3a0883824a7da4d2ed4d739444ba792cd778bb
Shahid-Zada <Shahidzada.cs@gmail.com>
2024-03-25 00:02:47 +0500

    tmbd api setup

---
+++ lib/commons/util.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const apiKey = "131169cd679790a6914256c9a1322692";
+const Color kbackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const imageUrl = "http://image.tmbd.org/t/p/w500";
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/commons/util.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,7 +11,39 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: kbackgroundColor,
+        title: Image.asset(
+          'assets/logo.png',
+          height: 50,
+          width: 120,
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: InkWell(
+              onTap: () {},
+              child: const Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              height: 27,
+              width: 27,
+              color: Colors.blue,
+            ),
+          ),
+          const SizedBox(
+            width: 20,
+          )
+        ],
+      ),
       body: Center(
         child: Text('Home screen'),
       ),
