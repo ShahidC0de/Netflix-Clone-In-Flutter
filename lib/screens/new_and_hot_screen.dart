aca4af0cd0f6e70dad9d9edb23efa148521a891a
Shahidc0de <shahidzada.cs@gmail.com>
2024-06-09 16:17:15 +0500

    security actions

--- lib/screens/new_and_hot_screen.dart
+++ lib/screens/new_and_hot_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+
 import 'package:netflix_clone/widgets/upcomingMovieWidget.dart';
 
 class MoreScreen extends StatefulWidget {
