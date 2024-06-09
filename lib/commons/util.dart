6edd73296d364bcddc4e449bcea4eba6ef211505
Shahidc0de <shahidzada.cs@gmail.com>
2024-06-09 14:14:11 +0500

    security actions

--- lib/commons/util.dart
+++ lib/commons/util.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const apiKey = "131169cd679790a6914256c9a1322692";
+// ignore: non_constant_identifier_names
+final API_key = dotenv.env['API_KEY'] ?? "";
+
+String apiKey = API_key;
 const Color kbackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const imageUrl = "http://image.tmdb.org/t/p/w500/";
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:netflix_clone/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.dark,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
-            .copyWith(background: Colors.black),
+            .copyWith(surface: Colors.black),
         useMaterial3: true,
         textTheme: const TextTheme(
             bodyLarge: TextStyle(
